68bb42bcfc0c258fe1c96da3471f394923fb2901
mynameisgoku <gokuthechosenone@gmail.com>
2017-06-22 01:24:13 +0000

    refactord motd file with a template resource

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -14,8 +14,7 @@ package 'git' do
    action :install
 end
 
-file '/etc/motd' do
-   content 'Property of Tyler Davis'
-   owner 'root'
-   group 'root'
+template '/etc/motd' do
+   source 'motd.erb'
+   action :create
 end
